f0bbca2415b43e82cb607bd2597ef041504619e9
fikrihaekal22 <67132372+fikrihaekal22@users.noreply.github.com>
2020-11-27 11:00:58 +0700

    Progres 1
    template login sama main

---
+++ ci_sikad/app/Controllers/Auth.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controllers;
+
+class Auth extends BaseController
+{
+    public function index()
+    {
+        $data = [
+            'title' => 'Login',
+            'isi' => 'v_login',
+        ];
+        return view('layout/v_wrapper', $data);
+    }
+
+    //--------------------------------------------------------------------
+
+}
---
+++ ci_sikad/app/Views/v_home.php
@@ -0,0 +1,10 @@
+<div class="row">
+    <div class="col-sm-12">
+        <div class="text-center">
+            <h2><b>Sistem Informasi Akademik ALIHA UNIVERSITY</b></h2>
+        </div>
+    </div>
+    <div class="col-sm-12 text-center">
+        <img class="img-responsive pad" src="<?= base_url('gambar/akademik.png') ?>" alt="Photo">
+    </div>
+</div>
